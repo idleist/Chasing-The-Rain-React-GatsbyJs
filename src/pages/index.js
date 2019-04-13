80d3d8747d7e27e29dc64ff15dd11e1e8d87f8b1
Ben Rugman <benrugman@yahoo.co.uk>
2019-04-13 11:31:59 +0100

    completed Navbar component and added to layout js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,48 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../layouts/layout"
 import SEO from "../components/seo"
+import books from "../assets/images/compositebooks.png"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO
+      title="Home"
+      keywords={[
+        `novel`,
+        `chasing the rain`,
+        `james rugman`,
+        "debut",
+        "fiction",
+      ]}
+    />
+    <div id="home">
+      <div id="home--title">
+        <h1 id="home--heading" className="home--heading">
+          Chasing The Rain
+        </h1>
+        <p id="home--tagline">By James E Rugman</p>
+      </div>
+      <div id="home--image">
+        <img id="home--image-formats" src={books} alt="book formats" />
+      </div>
+      <div id="home--buttons">
+        <Link to="/author" className="link--button">
+          The Author
+        </Link>
+        <Link to="/story" className="link--button">
+          The Story
+        </Link>
+        <a
+          href="https://www.amazon.co.uk/Chasing-Rain-James-Rugman/dp/1797415689/ref=la_B07PNMDJTD_1_1?s=books&amp;ie=UTF8&amp;qid=1553780744&amp;sr=1-1"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="linkButton"
+        >
+          Buy Now On Amazon
+        </a>
+      </div>
+    </div>
   </Layout>
 )
 
